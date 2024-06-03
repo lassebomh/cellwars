6cedc7076ae6dc6eaf70fa73a6656c718eac09cc
Lasse H. Bomholt <lasse@bomh.net>
2024-06-03 13:54:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,15 +39,6 @@ def pmat(mat):
 def blank_world(w, h):
     return np.zeros((w, h), np.int16)
 
-world = blank_world(6, 6)
-
-
-# world[-1, -1] = players[1]
-# world[-2, -1] = players[1]
-world[1, 1] = players[0]
-world[1, 2] = players[0]
-
-pmat(world)
 
 def resolve_combat(world):
     w,h = world.shape
@@ -76,52 +67,83 @@ def resolve_combat(world):
         
     return world    
 
-w,h = world.shape
 
-flow = np.zeros((w, h), world.dtype)
-flow[1, 1] = 3
-flow[1, 2] = 1
+def resolve_movmement(world, flow):
 
-pmat(flow)
+    w,h = world.shape
 
-indicies = np.full_like(flow, -1)
+    targets = defaultdict(list)
 
-targets = defaultdict(list)
-
-for x in range(w):
-    for y in range(h):
-        
-        if world[x, y] == 0:
-            continue
-        
-        d = flow[x, y]
-        
-        if (x == 0 and d == 4) or (x == w-1 and d == 2) or (y == 0 and d == 3) or (y == h-1 and d == 1):
-            d = 0
-    
-        nx, ny = x, y
-    
-        if d == 1:
-            ny -= 1
-        elif d == 3:
-            ny += 1
-        elif d == 2:
-            nx += 1
-        elif d == 4:
-            nx -= 1
+    for x in range(w):
+        for y in range(h):
             
-        index = ny * w + nx
-    
-        targets[index].append((x, y))
-    
-        # indicies[x, y] = 
-
-for i, positions in targets.items():
-    
-    curr = positions[0]
-    
-    while len(positions) > 1:
+            if world[x, y] == 0:
+                continue
+            
+            d = flow[x, y]
+            
+            if (x == 0 and d == 4) or (x == w-1 and d == 2) or (y == 0 and d == 3) or (y == h-1 and d == 1):
+                d = 0
         
+            nx, ny = x, y
+        
+            if d == 1:
+                ny -= 1
+            elif d == 3:
+                ny += 1
+            elif d == 2:
+                nx += 1
+            elif d == 4:
+                nx -= 1
+                
+            targets[(nx, ny)].append((x, y))
+        
+    new_world = np.zeros_like(world)
 
-print(targets)
-# pmat(indicies)
+    def fixblock(x, y):
+        positions = targets[x, y]
+        
+        while len(positions) > 1:
+            pos = positions.pop()
+            targets[pos].insert(0, pos)
+            fixblock(*pos)
+
+    for x, y in list(targets.keys()):
+        fixblock(x,y)
+        
+    for x, y in list(targets.keys()):
+        from_positions = targets[x, y]
+        if len(from_positions) == 0: continue
+        from_x, from_y = from_positions[0]
+        cell = world[from_x, from_y]
+        new_world[x, y] = cell
+        
+    return new_world
+
+
+world = blank_world(16, 16)
+
+world[-1, -1] = players[1]
+world[-1, -2] = players[1]
+world[1, 1] = players[0]
+world[1, 2] = players[0]
+world[1, 3] = players[0]
+# world[1, 4] = players[0]
+
+world = resolve_combat(world)
+
+pmat(world)
+
+flow = np.zeros_like(world)
+flow[1, 1] = 2
+flow[1, 2] = 2
+flow[1, 3] = 2
+# flow[1, 4] = 2
+
+world = resolve_movmement(world, flow)
+
+pmat(world)
+
+world = resolve_combat(world)
+
+pmat(world)
